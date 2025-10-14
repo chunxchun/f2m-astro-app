8480adec6d100574de977a96b02afd01b1875138
William Wu <chun@chunxchun.com>
2025-10-14 15:38:53 +0800

    add tailwind react shadcn

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,27 @@
 // @ts-check
 
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
 
-import cloudflare from '@astrojs/cloudflare';
+import cloudflare from "@astrojs/cloudflare";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap()],
+  site: "https://www.find2meals.com",
+  integrations: [mdx(), sitemap(), react()],
+
   adapter: cloudflare({
     platformProxy: {
-      enabled: true
+      enabled: true,
     },
 
-    imageService: "cloudflare"
+    imageService: "cloudflare",
   }),
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
