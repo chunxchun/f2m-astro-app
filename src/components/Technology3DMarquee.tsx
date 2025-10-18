7d5c14c48a195761eb4f5a0390c9decf07be8991
William Wu <chun@chunxchun.com>
2025-10-18 16:07:02 +0800

    update content

---
+++ src/components/Technology3DMarquee.tsx
@@ -0,0 +1,24 @@
+import { ThreeDMarquee } from "@/components/ui/shadcn-io/3d-marquee";
+export default function ThreeDMarqueeDemo() {
+  const images = [
+    // "https://assets.aceternity.com/cloudinary_bkp/3d-card.png",
+    // "https://assets.aceternity.com/animated-modal.png",
+    // "https://assets.aceternity.com/animated-testimonials.webp",
+    // "https://assets.aceternity.com/cloudinary_bkp/Tooltip_luwy44.png",
+    "https://assets.aceternity.com/github-globe.png",
+    "https://assets.aceternity.com/glare-card.png",
+    "https://assets.aceternity.com/layout-grid.png",
+    "https://assets.aceternity.com/flip-text.png",
+    "https://assets.aceternity.com/hero-highlight.png",
+    "https://assets.aceternity.com/carousel.webp",
+    "https://assets.aceternity.com/placeholders-and-vanish-input.png",
+    "https://assets.aceternity.com/shooting-stars-and-stars-background.png",
+    "https://assets.aceternity.com/signup-form.png",
+  ];
+
+  return (
+    <div className="mx-auto my-10 max-w-7xl rounded-3xl bg-gray-950/5 p-2 ring-1 ring-neutral-700/10 dark:bg-neutral-800">
+      <ThreeDMarquee images={images} />
+    </div>
+  );
+}
